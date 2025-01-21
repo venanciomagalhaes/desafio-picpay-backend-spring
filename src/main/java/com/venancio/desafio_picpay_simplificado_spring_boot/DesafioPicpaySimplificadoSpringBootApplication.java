b295ba5c51dbd825f7bb519fa12acff58f279a03
D.Venancio <venanciomagalhaesd@gmail.com>
2025-01-20 22:39:41 -0300

    commit inicial

---
+++ src/main/java/com/venancio/desafio_picpay_simplificado_spring_boot/DesafioPicpaySimplificadoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.venancio.desafio_picpay_simplificado_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPicpaySimplificadoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPicpaySimplificadoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venancio/desafio_picpay_simplificado_spring_boot/DesafioPicpaySimplificadoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venancio.desafio_picpay_simplificado_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpaySimplificadoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
