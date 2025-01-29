f77c2cf118b575c79e49c4a8ae13a64a3bd03d0e
D.Venancio <venanciomagalhaesd@gmail.com>
2025-01-29 13:21:44 -0300

    adicionando entidade de Transaction

---
+++ src/main/java/com/venancio/desafio_picpay_simplificado_spring_boot/domain/entities/Transaction.java
@@ -0,0 +1,62 @@
+package com.venancio.desafio_picpay_simplificado_spring_boot.domain.entities;
+
+import com.venancio.desafio_picpay_simplificado_spring_boot.domain.enums.TransferStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "payer_id", nullable = false)
+    private User payer;
+
+    @ManyToOne
+    @JoinColumn(name = "payee_id", nullable = false)
+    private User payee;
+
+    private BigDecimal value;
+
+    @Column(nullable = false)
+    private TransferStatus status;
+
+    @Column(nullable = false, name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    /**
+     * Define os valores de `createdAt` e `updatedAt` antes de persistir o objeto.
+     */
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    /**
+     * Atualiza o valor de `updatedAt` antes de atualizar o objeto.
+     */
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
