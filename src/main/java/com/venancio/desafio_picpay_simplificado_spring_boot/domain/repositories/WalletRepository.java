6f391848111f1f0ac831a957a959fe7deb00d9f5
D.Venancio <venanciomagalhaesd@gmail.com>
2025-01-30 19:13:41 -0300

    adicionando anotations de repository pendente

--- src/main/java/com/venancio/desafio_picpay_simplificado_spring_boot/domain/repositories/WalletRepository.java
+++ src/main/java/com/venancio/desafio_picpay_simplificado_spring_boot/domain/repositories/WalletRepository.java
@@ -2,6 +2,8 @@ package com.venancio.desafio_picpay_simplificado_spring_boot.domain.repositories
 
 import com.venancio.desafio_picpay_simplificado_spring_boot.domain.entities.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
 }
